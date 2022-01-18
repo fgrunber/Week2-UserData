8376426547365b387da8bb0b026b79a56c1343d6
fgrunber <96977461+fgrunber@users.noreply.github.com>
2022-01-18 12:33:47 +0200

    Control Flow

---
+++ UserData/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UserData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sünniaastat;
+            //programm arvutab kasutaja vanust;
+            //programm kuvab kasutaja vanust konsoolis;
+
+            Console.WriteLine("Palun, siseta oma sünniaasta:");
+            int YearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2021 - YearOfBirth;
+            //Console.WriteLine("Oled " + userAge + " aastat vana.");
+
+            //string interpolation - saab kirjutada täislauseid kuhu saab lisada muutujaid.
+
+            Console.WriteLine($"Oled {userAge} aastat vana.");
+        }
+    }
+}
